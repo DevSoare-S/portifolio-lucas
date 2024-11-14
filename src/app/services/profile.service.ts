aee795ba4be4baf176af63a16f8d39ac7b2ff256
Lucas <lucassoares.dev@hotmail.com>
2024-11-14 19:36:56 -0300

    Criando serviços e models

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Profile } from '../models/profile';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor() { }
+
+  getProfile(): Profile {
+    
+    var profile: Profile = {
+
+      email: 'lucassoaresdev.com.br',
+      name: 'Lucas Soares',
+      whatsAppUrl: 'https://api.whatsapp.com/send?phone=5554992980674&text=Ol%C3%A1,%20Gostaria%20de%20saber%20mais%20sobre%20seus%20servi%C3%A7os.',
+      linkedIn: 'https://www.linkedin.com/in/lucas-soares-265358328/?trk=opento_sprofile_goalscard'
+    }
+
+    return profile;
+  }
+}
